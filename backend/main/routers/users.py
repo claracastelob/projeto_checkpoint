6b685897e023f35a8ece8a55ef0530928a40212a
Clara Castelo Branco <castelob.clara@gmail.com>
2025-03-25 16:30:56 -0300

    Melhoria na autenticação e implementação de autorização dos usuários

--- backend/main/routers/users.py
+++ backend/main/routers/users.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 from main.database import get_session
 from main.models import User
 from main.schemas import (Message, UserList, UserPublic, UserSchema)
-from main.security import get_password_hash
+from main.security import get_password_hash, get_current_user
 
 router = APIRouter(prefix='/users', tags=['users'])
 
@@ -45,35 +45,29 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
   return db_user
 
 @router.put('/{user_id}', response_model=UserPublic)
-def update_user(user_id:int, user: UserSchema, session: Session = Depends(get_session)):
-  db_user = session.scalar(select(User).where(User.id == user_id))
+def update_user(
+  user_id:int, user: UserSchema,
+  session: Session = Depends(get_session),
+  current_user=Depends(get_current_user)
+):
+  if current_user.id != user_id:
+    raise HTTPException(status_code=400, detail='Not enough permission')
 
-  if not db_user:
-    raise HTTPException(
-      status_code = HTTPStatus.NOT_FOUND,
-      detail='User not found on database'
-    )
-  
-  db_user.user = user.user
-  db_user.email = user.email
-  db_user.password = get_password_hash(user.password)
+  current_user.user = user.user
+  current_user.email = user.email
+  current_user.password = get_password_hash(user.password)
 
-  session.add(db_user)
+  session.add(current_user)
   session.commit()
-  session.refresh(db_user)
+  session.refresh(current_user)
 
-  return db_user
+  return current_user
 
 @router.delete('/{user_id}', response_model=Message)
-def delete_user(user_id: int, session: Session = Depends(get_session)):
-  db_user = session.scalar(select(User).where(User.id == user_id))
-
-  if not db_user:
-    raise HTTPException(
-      status_code = HTTPStatus.NOT_FOUND,
-      detail='User not found on database'
-    )
+def delete_user(user_id: int, session: Session = Depends(get_session), current_user=Depends(get_current_user)):
+  if current_user.id != user_id:
+    raise HTTPException(status_code=400, detail='Not enough permission')
   
-  session.delete(db_user)
+  session.delete(current_user)
   session.commit()
   return {'message': 'User deleted from database'}
