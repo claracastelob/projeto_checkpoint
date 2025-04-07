7e759211c790b0a6e639e5f69c1033d176412564
Clara Castelo Branco <castelob.clara@gmail.com>
2025-04-07 18:52:29 -0300

    Melhorias na tela de biblioteca do usuário e criação da documentação inicial do projeto

---
+++ README.md
@@ -0,0 +1,45 @@
+# Projeto Checkpoint
+Este projeto foi pensado como um estudo na área de programação envolvendo tanto o backend como o frontend. O objetivo é ter contato com novas tecnologias, além de expandir meus conhecimentos na construção de aplicações e sistemas.  
+Essas são algumas coisas que pude estudar e aprender até agora durante esse processo:
+- Autenticação e autorização com OAuth2
+- Criptografia de dados 
+- Manipulação de rotas no React
+- Criação e manipulação de tabelas e banco de dados
+
+### Descrição
+O checkpoint consiste em uma aplicação parecida com um "letterboxd" mas voltada para jogos, ou seja, você pode compartilhar suas experiências nas gameplays, contando o que achou do jogo, em quanto tempo finalizou, a sua nota final e etc.
+
+### Funcionalidades
+Principais funcionalidades da aplicação.
+
+- Cadastro e login de usuários com JWT
+- Proteção de rotas autenticadas
+- Alterações de dados por parte dos usuários
+
+### Tecnologias Utilizadas
+Essas são as tecnologias/ferramentas usadas nesse projeto.
+
+**Backend**
+- Python
+- FastAPI
+- SQLAlchemy
+- Poetry
+- Alembic
+- Docker
+
+**Frontend**
+- React
+- Node.js
+- Axios
+- Vite
+
+**Banco de Dados**
+- Mysql
+
+### Próximos Passos
+Essa aplicação ainda está em desenvolvimento. Abaixo estão algumas das funcionalidades planejadas:  
+- [x] Melhorias visuais  
+- [ ] Implementação das rotas e banco de dados para a biblioteca de jogos dos usuários  
+- [ ] Integração com uma API externa para fornecer informações sobre os jogos
+- [ ] Dockerização completa do projeto  
+- [ ] Funcionalidade de envio de e-mails (recuperação de senha, confirmação de cadastro)  
--- frontend/src/components/NewGameModal/NewGameModal.jsx
+++ frontend/src/components/NewGameModal/NewGameModal.jsx
@@ -8,17 +8,32 @@ export default function NewGameModal({isOpen, onClose}) {
         <div className={styles.container}>
           <button className={styles.closeBtn} onClick={onClose}>X</button>
           <h2>Adicionar Novo Jogo</h2>
-          <form action="">
-            <label htmlFor="nameGame">Digite o nome do jogo:</label>
-            <input type="text" id="nameGame" />
-            <label htmlFor="experience">Como foi sua experiência?</label>
-            <input type="text" id="experience" />
-            <label htmlFor="improve">O que poderia ter sido melhor?</label>
-            <input type="text" id="improve" />
-            <label htmlFor="toc">Tempo de conclusão (horas)</label>
-            <input type="number" id="toc" />
-            <label htmlFor="note">Nota Geral (1 - 5)</label>
-            <input type="number" id="note" />
+          <form>
+            <div className={styles.formGroup}>
+              <label htmlFor="nameGame">Digite o nome do jogo:</label>
+              <input type="text" id="nameGame" />
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="experience">Como foi sua experiência?</label>
+              <input type="text" id="experience" />
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="improve">O que poderia ter sido melhor?</label>
+              <input type="text" id="improve" />
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="toc">Tempo de conclusão (horas)</label>
+              <input type="number" id="toc" />
+            </div>
+
+            <div className={styles.formGroup}>
+              <label htmlFor="note">Nota Geral (1 - 5)</label>
+              <input type="number" id="note" />
+            </div>
+
             <button className={styles.addGame}>Adicionar</button>
           </form>
 
